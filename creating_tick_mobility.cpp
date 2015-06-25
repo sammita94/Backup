ef48dcd61acfd27135e6c2658fbd8486421e2a2c
sweetsammy <sammitachakravarti@yahoo.com>
2015-06-25 12:00:36 +0530

    added comments to creating_tick_mobility

--- creating_tick_mobility.cpp
+++ creating_tick_mobility.cpp
@@ -1,19 +1,22 @@
+/*Program to generate the co-ordinates of the clients at all the tick-times*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
 #define PI 3.14159265
 
+/*for getting the time from the line*/
 double gettime(string line)
 {
     int i=0;
     string res;
-    //cout<<line<<"in gettime";
     while(line[i]!=' ')
         res+=line[i++];
     double s=stod(res,nullptr);
     return s;
 }
 
+/*for getting the co-ordinates*/
 pair <double,double> get_xy(string line)
 {
     int i=0,co_space=0;
@@ -36,6 +39,7 @@ pair <double,double> get_xy(string line)
 
 }
 
+/*for writing to the output file*/
 void write_output_file(int client_id, double time, pair <double, double> point)
 {
     string str="client_mob_";
@@ -47,6 +51,7 @@ void write_output_file(int client_id, double time, pair <double, double> point)
     work.close();
 }
 
+/*generates the next point*/
 pair<double,double> generate_next_point(pair<double,double> point,double angle,double dist)
 {
     pair<double,double> next_point;
@@ -58,23 +63,26 @@ pair<double,double> generate_next_point(pair<double,double> point,double angle,d
 int main(int argc, char *argv[])
 {
     if(argc!=3)
+    {
+        printf("Usage: Compile the program under stdc++11. Then give the command \a.out <No.of clients> <speed>");
         exit(0);
+    }
 
     int no_of_clients=stoi(argv[1],nullptr,10);
-    //cout<<no_of_clients;
     float speed=stof(argv[2],nullptr);
+
     int present_client,no_of_lines;
     string line;
 
+    /*The tick time..... can be adjusted by changing*/
     float percent_set_up=0.15;
     float tick_time=0.00137;
     tick_time=(1+percent_set_up)*tick_time;
 
     for(present_client=0;present_client<no_of_clients;present_client++)
     {
-
-        ifstream client_read_handle;
-        ifstream client_read_lines_handle;
+        ifstream client_read_handle;//for reading the points
+        ifstream client_read_lines_handle;//for reading the no.of lines in each client file
         string str="client_";
         str=str+to_string(present_client);
         str=str+".txt";
@@ -87,59 +95,56 @@ int main(int argc, char *argv[])
         str="client_mob_";
         str=str+to_string(present_client);
         str=str+".txt";
-        ofstream work;
+        ofstream work;//for writing the points at various instants
         work.open(str.c_str());
 
+        /*calculating the no.of co-ordinates given in the file*/
         while(getline(client_read_lines_handle,line))
             no_of_lines++;
 
-        //cout<<no_of_lines<<endl;
-
-        double start_time=0.0,finish_time,next_time,current_time;
+        double start_time=0.0,finish_time,current_time,dist_total,angle,dist,dist_trial;
         pair <double,double> co_or_start,co_or_finish,trial_point;
-        double m=0.0,dist_total,dist,dist_trial,angle;
+
         dist = tick_time * speed;
-        getline(client_read_handle,line);
+        getline(client_read_handle,line);//reading the line from the client file
         no_of_lines--;
+
         start_time = gettime(line);
         co_or_start= get_xy(line);
         while(no_of_lines--)
         {
+            getline(client_read_handle,line);
+            co_or_finish = get_xy(line);//gets the x and y co-ordinates
+            trial_point = generate_next_point(co_or_start,angle,dist);//generates the next point
 
+            dist_trial = pow((co_or_start.first-trial_point.first),2)+pow((co_or_start.second-trial_point.second),2);
+            dist_trial = sqrt(dist_trial);
 
-                getline(client_read_handle,line);
-                //if(present_client==1)
-                    //cout<<line<<endl;
-                co_or_finish = get_xy(line);
-                trial_point = generate_next_point(co_or_start,angle,dist);
-                dist_trial = pow((co_or_start.first-trial_point.first),2)+pow((co_or_start.second-trial_point.second),2);
-                dist_trial = sqrt(dist_trial);
-                dist_total = pow((co_or_finish.first-co_or_start.first),2)+pow((co_or_finish.second-co_or_start.second),2);
-                dist_total = sqrt(dist_total);
-                current_time = start_time;
-                finish_time = gettime(line);
-                angle = (co_or_finish.second-co_or_start.second)/(co_or_finish.first-co_or_start.first);
-                angle = atan (angle) * 180 / PI;
+            dist_total = pow((co_or_finish.first-co_or_start.first),2)+pow((co_or_finish.second-co_or_start.second),2);
+            dist_total = sqrt(dist_total);
 
-                while(dist_trial<=dist_total)
-                {
-                    //if(present_client==1)
-                        //cout<<trial_point.first<<" "<<trial_point.second<<endl;
-                    work<<to_string(trial_point.first)<<" "<<to_string(trial_point.second)<<"\n";
-                    current_time = current_time + tick_time;
-                    trial_point = generate_next_point(trial_point,angle,dist);
-                    dist_trial = dist_trial+dist;
-                //cout<<dist_trial<<"..."<<dist_total<<endl;
-                }
-                co_or_start=co_or_finish;
-                start_time = finish_time;
+            current_time = start_time;
+            finish_time = gettime(line);
 
+            angle = (co_or_finish.second-co_or_start.second)/(co_or_finish.first-co_or_start.first);
+            angle = atan (angle) * 180 / PI;
+
+            while(dist_trial<=dist_total)
+            {
+                work<<to_string(trial_point.first)<<" "<<to_string(trial_point.second)<<"\n";//writing the co-ordinates to the mobility file of the client
+                current_time = current_time + tick_time;
+
+                trial_point = generate_next_point(trial_point,angle,dist);//generates the next point
+                dist_trial = dist_trial+dist;
             }
-            client_read_handle.close();
-            client_read_lines_handle.close();
-            work.close();
 
+            co_or_start = co_or_finish;
+            start_time = finish_time;
+
+        }
+        client_read_handle.close();
+        client_read_lines_handle.close();
+        work.close();
     }
     return 0;
-
 }
